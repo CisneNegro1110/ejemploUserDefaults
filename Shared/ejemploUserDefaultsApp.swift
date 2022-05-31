c8b550dba3da34973f607191f3f7d56e2a3f8930
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-05-30 19:20:17 -0600

    Initial Commit

---
+++ Shared/ejemploUserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejemploUserDefaultsApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ejemploUserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
